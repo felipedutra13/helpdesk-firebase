23109ea9cb1b10915ca7302daf25d5c107bc7eca
felipedutra13 <dutra.felipe92@gmail.com>
2022-08-30 08:52:37 -0300

    project finished

---
+++ .history/src/components/Forms/AccountForm/index_20220817164037.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
+
+import { Button } from '@components/Controllers/Button';
+import { Input } from '@components/Controllers/Input';
+import { Form, Title } from './styles';
+import auth from '@react-native-firebase/auth';
+
+export function AccountForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleNewAccount() {
+    setIsLoading(true);
+
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => Alert.alert("Conta", "Cadastrado com sucesso!"))
+      .catch((error) => console.log(error));
+  }
+
+  return (
+    <Form>
+      <Title>Cadastrar</Title>
+      <Input placeholder="E-mail" onChangeText={setEmail} />
+      <Input placeholder="Senha" secureTextEntry onChangeText={setPassword} />
+      <Button title="Cadastrar" isLoading={isLoading} onPress={handleNewAccount} />
+    </Form>
+  );
+}
